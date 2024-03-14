58bf51aba12a4cb19e570f41b95f0af1a16c0e80
Akshat Gautam <ronitakki3@gmail.com>
2024-03-14 19:30:19 +0530

    Added Custom Cursor

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import Contact from './Components/Contact';
 import ScrollToTopButton from './Components/ScrollToTopButton';
 import Loading from './Components/Loading';
 import About from './Components/About';
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
     return (
         <>
+            <AnimatedCursor
+                innerSize={8}
+                outerSize={30}
+                innerScale={1}
+                outerScale={2}
+                outerAlpha={0.2}
+                hasBlendMode={true}
+                trailingSpeed={6}
+                innerStyle={{
+                    backgroundColor: '#fff', zIndex: '99999'
+                }}
+                outerStyle={{
+                    border: '3px solid #02BCC3', zIndex: '99999'
+                }}
+            />
             <Loading />
             <Navbar />
             <Hero />
