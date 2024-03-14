07fbf3734d5b2bca7d38fe07f68dcaf420d38bc3
Akshat Gautam <ronitakki3@gmail.com>
2024-03-14 17:46:14 +0530

    Added Project Links and Fixed Navigation Bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,124 +2,157 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
 import logo from '../Images/logo.png';
 import { Link } from 'react-scroll';
+import { Drawer } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const pages = ['home', 'about', 'projects', 'contact'];
 
 export default function Navbar() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
+    
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
     };
 
     return (
         <AppBar position="fixed" color="transparent" style={{ boxShadow: 'none', backdropFilter: 'blur(18px) brightness(70%)' }} >
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <img width={70} src={logo} alt=''/>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'Inter',
-                            fontWeight: 600,
-                            fontSize: '1.5rem',
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Akshat Gautam
-                    </Typography>
+                    <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+                        <img width={70}  src={logo} alt='' style={{ marginRight: 10 }} onClick={handleOpenNavMenu} />
+                    </Box>
+                    <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+                        <img width={70} src={logo} alt='' style={{ marginRight: 10 }} />
+                    </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'Inter',
+                                fontWeight: 500,
+                                fontSize: '1.5rem',
+                                color: 'white',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Akshat Gautam
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            sx={{
+                                pr: 2.5,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'Caveat',
+                                fontWeight: 400,
+                                fontSize: '1.3rem',
+                                color: 'white',
+                                alignSelf: 'flex-end',
+                                lineHeight: 1
+                            }}
+                        >
+                            ~ Code is poetry
+                        </Typography>
+                    </Box>
+
+
+
 
                     {/* Menu Icon and Sidebar controls */}
-                    {/* <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">{page}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box> */}
-
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
+                    <Drawer open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} PaperProps={{ sx: { backgroundColor: 'whitesmoke' } }}>
+                        <CloseIcon sx={{ fontSize: 30, mx: 3, my: 4 }} onClick={handleCloseNavMenu} />
+                        {pages.map((page) => (
+                            <Box sx={{ width: '20em' }}>
+                                <Button
+                                    key={page}
+                                    sx={{ my: 0.5, mx: 3, color: 'black', display: 'block', textTransform: 'lowercase', fontFamily: 'Inter', fontSize: '1rem', fontWeight: '600', letterSpacing: '-0.5px' }}
+                                >
+                                    <Link
+                                        onClick={handleCloseNavMenu}
+                                        to={page}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        {"// " + page}
+                                    </Link>
+                                </Button>
+                            </Box>
+                        ))}
+                        <Typography sx={{
                             fontFamily: 'Inter',
-                            fontWeight: 600,
-                            fontSize: '1.5rem',
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Akshat Gautam
-                    </Typography>
+                            fontSize: '0.8rem',
+                            marginLeft: 4,
+                            my: 4,
+                            lineHeight: 2
+                        }}>©2024<br />Made with passion by <Typography sx={{
+                            display: 'inline',
+                            fontFamily: 'Caveat',
+                            fontSize: '1.2rem',
+                            fontWeight: 'bolder'
+                        }}>Akshat Gautam</Typography></Typography>
+                    </Drawer>
+
+
+
+
+                    <Box sx={{ display: 'flex', flexDirection: 'column', my: 1.5 }}>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'Inter',
+                                fontWeight: 500,
+                                fontSize: '1.5rem',
+                                color: 'white',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Akshat Gautam
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            sx={{
+                                pr: 2.5,
+                                display: { xs: 'flex', md: 'none' },
+                                fontFamily: 'Caveat',
+                                fontWeight: 400,
+                                fontSize: '1.3rem',
+                                color: 'white',
+                                alignSelf: 'flex-end',
+                                lineHeight: 1
+                            }}
+                        >
+                            ~ Code is poetry
+                        </Typography>
+                    </Box>
+
+
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={handleCloseNavMenu}
                                 sx={{ my: 2, mx: 2, color: '#02BCC3', display: 'block', textTransform: 'lowercase', fontFamily: 'Inter', fontSize: '1.1rem', fontWeight: '500', letterSpacing: '-0.5px' }}
                             >
                                 <Link
@@ -129,13 +162,13 @@ export default function Navbar() {
                                     offset={-70} // Adjust this value to offset the scroll position
                                     duration={500}
                                 >
-                                    {"/ / " + page}
+                                    {"// " + page}
                                 </Link>
                             </Button>
                         ))}
                     </Box>
 
-                    <Box sx={{ flexGrow: 0.3 }}>
+                    <Box sx={{ flexGrow: 0.4 }}>
                     </Box>
                 </Toolbar>
             </Container>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -13,21 +13,25 @@ export default function Projects() {
             name: "Real-Time Chat Web App",
             description: "Web Application made to send and recieve messages in real-time using Socket.io. Learnt about real-time communication",
             image: project1pic,
+            link: 'https://gutur-gu.onrender.com/'
         },
         {
             name: "Note Keeping Web App",
             description: "Web Application made to store notes on cloud. Made using the MERN Stack. Learnt a lot about State Management and REST APIs.",
             image: project2pic,
+            link: 'https://dont-keep-frontend.onrender.com/'
         },
         {
             name: "News Application",
             description: "Web Application made to read daily news fetched from NewsAPI’s API. Learnt about API calls, displaying JSON response.",
-            image: project4pic
+            image: project4pic,
+            link: 'https://github.com/akshat2503/NewsMonkey'
         },
         {
             name: 'School Website',
             description: 'Static Website made for a local school. Using HTML, CSS and Bootstrap. Learnt about end user satisfaction and cooperation.',
             image: project3pic,
+            link: 'https://goldenarrows.in/'
         },
     ];
 
@@ -59,17 +63,21 @@ export default function Projects() {
 
                     <Card sx={{ maxWidth: 345, backgroundColor: 'black', mx: { xs: '', md: 3 }, my: { xs: 3, md: '' }, '&:hover': { boxShadow: '0px 0px 20px 0px rgba(255,255,255,0.5)', transition: 'box-shadow 0.3s ease-in-out' } }}>
                         <CardMedia
-                            sx={{ height: 190 }}
+                            sx={{ height: 190, cursor: 'pointer' }}
                             image={project.image}
                             title=""
+                            component='a'
+                            href={project.link}
+                            target='_blank'
                         />
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="div" color={"white"} fontFamily={'Inter'}>
                                 {project.name}
                             </Typography>
-                            <Typography variant="body2" color={"white"} sx={{ opacity: 0.7, fontFamily: 'Inter', fontStyle: 'italic' }} >
+                            <Typography variant="body2" color={"white"} sx={{ opacity: 0.7, fontFamily: 'Inter', fontStyle: 'italic', pb: 1 }} >
                                 {project.description}
                             </Typography>
+                            <Typography component='a' href={project.link} target='_blank' sx={{fontFamily: 'Inter', fontSize: '0.9rem', textDecoration: 'none', color: 'white', opacity: '0.7', '&:hover': {opacity: '1', transition: 'opacity 0.3s ease-in-out'}}}>Show Project</Typography>
                         </CardContent>
                     </Card>
                 ))}
